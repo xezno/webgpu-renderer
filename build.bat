6b11b83d4991b0b826a59ce885e848b493a4eed0
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2023-11-06 16:47:04 +0000

    Debug build

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 IF NOT EXIST build (mkdir build)
 cd build
 cmake .. -G "Visual Studio 17 2022" -A x64
-cmake --build . --config Release
-.\Release\HackweekWebGPU.exe
+cmake --build . --config Debug
+.\Debug\HackweekWebGPU.exe
 cd ..
