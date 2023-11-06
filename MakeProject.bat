e1b2d0b5260e276a0a39e59cd51f22529361e29f
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2023-11-06 17:25:53 +0000

    build.bat -> MakeProject.bat, don't build - use visual studio for this instead

--- MakeProject.bat
+++ MakeProject.bat
@@ -3,6 +3,3 @@ IF EXIST build(rmdir /s /q build)
 IF NOT EXIST build (mkdir build)
 cd build
 cmake .. -G "Visual Studio 17 2022" -A x64
-cmake --build . --config Debug
-.\Debug\HackweekWebGPU.exe
-cd ..
